9db735bbfb345e685220b4f173607e3e5efc209f
Leandro <Pimentel>
2025-11-24 12:12:08 +0200

    deleted everything i added

--- app.py
+++ app.py
@@ -357,6 +357,4 @@ def serve_static(filename):
     return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=True)
